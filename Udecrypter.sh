9805ab0ed84499dbb20adef08cf4a6a45b9bd5e7
Danish <66021632+danish-mar@users.noreply.github.com>
2021-06-08 02:01:42 +0530

    Update Udecrypter.sh

--- Udecrypter.sh
+++ Udecrypter.sh
@@ -1,31 +1,26 @@
 clear 
 
-echo "UDecrypter BOT Scripted by Mr.Den_Mar" | pv -qL 10
+echo "UDecrypter BOT Scripted by Mr.Den_Mar" 
 
-echo "Starting up UDecrypter Bot" | pv -qL 10
-echo "//////////////////////////" | pv -qL 5
+echo "Starting up UDecrypter Bot" 
+echo "//////////////////////////" 
 
-echo "Hello There i Am UDecrypter" | pv -qL 10
+echo "Hello There i Am UDecrypter" 
 
-echo "Please Tell me Which .Cap file you want to Decrypt ?" | pv -qL 10
+echo "Please Tell me Which .Cap file you want to Decrypt ?"  
 read -a cap
 sleep 1s
-echo "Ok, Now tell me which file did i use as a wordlist ?" | pv -qL 10
+echo "Ok, Now tell me which file did i use as a wordlist ?" 
 
 read -a wrdlst
 sleep 1s
-echo "Got it Now tell me what is the name of that WI-FI which is belongs to this cap file :" $cap | pv -qL 10
-
+echo "Got it Now tell me what is the name of that WI-FI which is belongs to this cap file :" $cap 
 read -a essid
 sleep 1s
-echo "Ok Stay Back and Chill I will take care of that And Make Sure Your Hardware are not overheating !!" | pv -qL 10
+echo "Ok Stay Back and Chill I will take care of that And Make Sure Your Hardware are not overheating !!" |
 
-echo "Starting Decryption in 5 seconds Using Aircrack-NG" | pv -qL 10
-echo "1" | pv -qL 1
-echo "2" | pv -qL 1
-echo "3" | pv -qL 1
-echo "4" | pv -qL 1
-echo "5" | pv -qL 1
+echo "Starting Decryption in 5 seconds Using Aircrack-NG" 
+
 touch key.txt
 
 echo "Starting!!" | pv -qL 10
