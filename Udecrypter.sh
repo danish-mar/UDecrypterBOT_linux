96004d2b690e7ac2947255c2727511cb68f99783
Dennis <66021632+danish-mar@users.noreply.github.com>
2021-07-21 01:45:57 +0530

    Update Udecrypter.sh

--- Udecrypter.sh
+++ Udecrypter.sh
@@ -17,7 +17,7 @@ sleep 1s
 echo "Got it Now tell me what is the name of that WI-FI which is belongs to this cap file :" $cap 
 read -a essid
 sleep 1s
-echo "Ok Stay Back and Chill I will take care of that And Make Sure Your Hardware are not overheating !!" |
+echo "Ok Stay Back and Chill I will take care of that And Make Sure Your Hardware are not overheating !!" 
 
 echo "Starting Decryption in 5 seconds Using Aircrack-NG" 
 
